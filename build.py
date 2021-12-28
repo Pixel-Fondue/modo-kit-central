caab40ae6d103fc17fe17fbaef247de9056f9a92
smoluck <smoluck@users.noreply.github.com>
2021-12-28 21:49:27 +0900

    Build LPK Bugfix Try01
    Tried to pack the files in LPK file via the build.py

--- build.py
+++ build.py
@@ -1,22 +1,76 @@
+import os
 from os import mkdir
 from pathlib import Path
 from shutil import rmtree
 from zipfile import ZipFile, ZIP_DEFLATED
 
 import utils
-from utils import make_index
+from utils import mid_header
+from utils import mid_root
+from utils import mid_configs
+from utils import mid_lxserv
+from utils import mid_message
+from utils import mid_resources
 
 # Get the root path to this repo
 repo_dir = Path(__file__).parent
-# Get the kit directory
+
+kit_name = "community_hub"
+# Get the kit directories
 kit_dir = repo_dir / "community_hub"
+configs_dir = kit_dir / "configs"
+lxserv_dir = kit_dir / "lxserv"
+resources_dir = kit_dir / "resources"
 # Get the build directory
 build_dir = repo_dir / "build"
 # Get the license file
 license_file = repo_dir / "LICENSE"
 
-# Get all files in the kit directory and male sure no pyc files come along
-kit_files = [f for f in kit_dir.glob("**/*") if f.is_file() and not f.name.endswith(".pyc")]
+
+# Get all files in the kit directory and make sure no pyc files come along.
+def root_fi():
+    # Get Base files for a kit description index.cfg
+    root_files = []
+    files = [f for f in os.listdir(kit_dir) if f.endswith(".cfg") or f.endswith(".txt")]
+    for f in files:
+        f = kit_dir / f
+        root_files.append(f)
+    # licen = [f for f in os.listdir(kit_dir) if f.endswith("cense")]
+    # for f in licen:
+    #     f = kit_dir / f
+    #     root_files.append(f)
+    print("INDEX ----- Root files -----")
+    print(root_files)
+    return root_files
+# root_fi()
+
+# Files contained in the "config" folder
+def configs_fi():
+    # Get Base files in folders "configs" and make sure no pyc files come along
+    configs_files = [f for f in configs_dir.glob("**/*") if f.is_file() and not f.name.endswith(".pyc")]
+    print("INDEX ----- files in configs folder -----")
+    print(configs_files)
+    return configs_files
+# config_fi()
+
+# Files contained in the "lxserv" folder and make sure no pyc files come along
+def lxserv_fi():
+    lxserv_files = [f for f in lxserv_dir.glob("**/*") if f.is_file() and not f.name.endswith(".pyc")]
+    print("INDEX ----- files in lxserv folder -----")
+    print(lxserv_files)
+    return lxserv_files
+# lxserv_fi()
+
+# Files contained in the "scripts" folder
+def resources_fi():
+    # Get Base files in folders "resources" and make sure no pyc files come along
+    resources_files = [f for f in resources_dir.glob("**/*") if f.is_file() and not f.name.endswith(".pyc")]
+    print("INDEX ----- files in resources folder -----")
+    print(resources_files)
+    return resources_files
+# resources_fi()
+
+
 
 # Clear the build directory
 if build_dir.exists():
@@ -33,13 +87,51 @@ message = f"Successfully installed Modo Community Hub: v{version}"
 # Build the LPK file.
 with ZipFile(lpk_path, mode="w", compression=ZIP_DEFLATED) as lpk:
     # Add the license
-    lpk.write(license_file, "license")
+    # lpk.write(license_file, "license")
+
     # Generate the index.xml file data
-    index_data = make_index(folder=kit_dir, files=kit_files, message=message)
+    indexheader = mid_header(name=kit_name, restart="YES")
+
+    ###### ModoComHub BASE STRUCTURE
+    print("----- Copy Root Data -----")
+    indexroot = mid_root(folder=kit_dir, files=root_fi())
+
+    print("----- Copy Root/configs Folder Data -----")
+    indexconfigs = mid_configs(folder=kit_dir, files=configs_fi())
+
+    print("----- Copy Root/lxserv Folder Data -----")
+    indexlxserv = mid_lxserv(folder=kit_dir, files=lxserv_fi())
+
+    print("----- Copy Root/resources Folder Data -----")
+    indexresources = mid_resources(folder=kit_dir, files=resources_fi())
+
+    ###### MESSAGE
+    indexmessage = mid_message(info=message)
+
     # Write the index.xml file
+    index_data = (indexheader + indexroot + indexconfigs + indexmessage)
+    # index_data = (indexheader + indexroot + indexconfigs + indexlxserv + indexresources + indexmessage)
     lpk.writestr("index.xml", index_data)
 
     # Write all file into the lpk
-    for file in kit_files:
+    print("----- Root files -----")
+    for file in root_fi():
         print(file.relative_to(kit_dir))
         lpk.write(file, file.relative_to(kit_dir))
+
+    print("----- configs folder files -----")
+    for file in configs_fi():
+        print(file.relative_to(kit_dir))
+        lpk.write(file, file.relative_to(kit_dir))
+
+    print("----- lxserv Folder files -----")
+    for file in lxserv_fi():
+        print(file.relative_to(kit_dir))
+        lpk.write(file, file.relative_to(kit_dir))
+
+    print("----- resources Folder files -----")
+    for file in resources_fi():
+        print(file.relative_to(kit_dir))
+        lpk.write(file, file.relative_to(kit_dir))
+
+
--- utils.py
+++ utils.py
@@ -3,35 +3,81 @@ from pathlib import Path
 # Get the root path to this repo
 repo_dir = Path(__file__).parent
 
-def make_index(folder, files, message, restart="No"):
-    """ Method to generate the body of an index.xml for packaging example files.
+""" Method to generate the body of an index.xml for packaging example files.
 
-    Args:
-        folder (Path): The name of the folder.
-        files (list(Path)): List of files in the folder.
-        message (str): The message to display to the user after installing the lpk.
-        restart (bool): If the lpk should present the restart message to the user.
+Args:
+    folder (Path): The name of the folder.
+    files (list(Path)): List of files in the folder.
+    message (str): The message to display to the user after installing the lpk.
+    restart (bool): If the lpk should present the restart message to the user.
 
-    Returns:
-        xml (str): the generated index.xml template.
-    """
+Returns:
+    xml (str): the generated index.xml template.
+"""
+
+###### Create the XML file header for the index.xml file  (packed in the lpk to unzip files to corresponding directory)
+###### mid stands for "MakeIndex"
+def mid_header(name, restart="No"):
     # Header
-    xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+    xml_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
     # Modo 10+
-    xml += "\n<package version=\"1000\">"
+    xml_header += "\n<package version=\"1000\">"
     # No need to restart
     restart = "YES" if restart else "NO"
-    xml += f'\n\t<kit name="{folder}" restart="{restart}">'
+    xml_header += f'\n\t<kit name="{name}" restart="{restart}">'
+    return xml_header
+
+def mid_root(folder, files):
     # For each file add target
+    xml_root = f''
     for file in files:  # type: Path
         # Path including the kit directory
-        full_path = file.relative_to(folder.parent)
+        root_full_path = file.relative_to(folder.parent)
         # Path without kit directory
-        rel_path = file.relative_to(folder)
-        xml += f'\n\t\t<source target="{full_path}">{rel_path}</source>'
-    xml += f'\n\t</kit>\n\t<message button="Help">{message}</message>\n</package>'
+        root_rel_path = file.relative_to(folder)
+        xml_root += f'\n\t\t<source target="{root_full_path}">{root_rel_path}</source>'
+    # print (xml_root)
     # Return Text
-    return xml
+    return xml_root
+
+def mid_configs(folder, files):
+    # For each file add target
+    xml_configs = f''
+    for file in files:  # type: Path
+        # Path including the kit directory
+        configs_full_path = file.relative_to(folder.parent)
+        # Path without kit directory
+        configs_rel_path = file.relative_to(folder)
+        xml_configs += f'\n\t\t<source target="{configs_full_path}">{configs_rel_path}</source>'
+    # print (xml_configs)
+    return xml_configs
+
+def mid_resources(folder, files):
+    # For each file add target
+    xml_resources = f''
+    for file in files:  # type: Path
+        # Path including the kit directory
+        resources_full_path = file.relative_to(folder.parent)
+        # Path without kit directory
+        resources_rel_path = file.relative_to(folder)
+        xml_resources += f'\n\t\t<source target="{resources_full_path}">{resources_rel_path}</source>'
+    # print (xml_resources)
+    return xml_resources
+
+def mid_lxserv(folder, files):
+    # For each file add target
+    xml_lxserv = f''
+    for file in files:  # type: Path
+        # Path including the kit directory
+        lxserv_full_path = file.relative_to(folder.parent)
+        # Path without kit directory
+        lxserv_rel_path = file.relative_to(folder)
+        xml_lxserv += f'\n\t\t<source target="{lxserv_full_path}">{lxserv_rel_path}</source>'
+    return xml_lxserv
+
+def mid_message(info):
+    xml_message = f'\n\t</kit>\n\t<message button="Help">{info}</message>\n</package>'
+    return xml_message
 
 
 def get_version():
