8fc8e1a00980eba662216a58b585fa11c56839c9
Shawn Frueh <shawnfrueh@live.com>
2025-04-07 16:43:07 -0500

    Use prefs Project in the build scripts

--- scripts/version.py
+++ scripts/version.py
@@ -1,18 +1,16 @@
 """Handles writing to GitHubs environment file."""
-import toml
 import os
+
+from .prefs import Project
 
 
 def set_env_vars() -> None:
     """Read version info from pyproject.toml and set GitHub environment variables."""
-    data = toml.load("pyproject.toml")
-    version = data['project']['version']
-    name = data['project']['name']
 
     # GitHub Actions environment file
     with open(os.environ['GITHUB_ENV'], 'a') as env_file:
-        env_file.write(f"KIT_VERSION={version}\n")
-        env_file.write(f"KIT_NAME={name}\n")
+        env_file.write(f"KIT_VERSION={Project.VERSION}\n")
+        env_file.write(f"KIT_NAME={Project.NAME}\n")
 
 
 if __name__ == '__main__':
